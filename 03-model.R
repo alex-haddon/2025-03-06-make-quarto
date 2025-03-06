4af8719326259fd5c920a1ca19829a7f1d6a377b
Alex Haddon <ahaddon@student.ubc.ca>
2025-03-06 12:56:50 -0800

    add report index.html

--- 03-model.R
+++ 03-model.R
@@ -14,6 +14,9 @@ model <- glm(survived ~ as.factor(pclass) + sex + age + fare,
   family = "binomial"
 )
 
+print("waiting for 5 seconds")
+Sys.sleep(5)
+
 summary(model)
 
 write_rds(model, opt$output_path)
